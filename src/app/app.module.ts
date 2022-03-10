04f70ad4b3356cd194724e36483daa67ae5e70df
khalil <khilel208@gmail.com>
2022-03-10 10:24:31 +0300

    work with api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StudentComponent } from './student/student.component';
+import { ShowStuComponent } from './student/show-stu/show-stu.component';
+import { AddEditStuComponent } from './student/add-edit-stu/add-edit-stu.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { AddEditDepComponent } from './department/add-edit-dep/add-edit-dep.component';
+import { HttpClientModule } from "@angular/common/http";
+import { SharedService } from "./shared.service";
+import { FormsModule,ReactiveFormsModule } from "@angular/forms";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    ShowStuComponent,
+    AddEditStuComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    AddEditDepComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
